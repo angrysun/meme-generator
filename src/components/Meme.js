39b480055d05ebbc8191725886eec2f950172a31
Steven Vasquez <s.vasquez85@gmail.com>
2022-05-03 10:50:29 +0900

    styling and forms added

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,7 +1,23 @@
 export default function Meme() {
   return (
-    <div>
-      <form>Hi</form>
-    </div>
+    <main>
+      <form className="form">
+        <input
+          type="text"
+          placeholder="Top text"
+          className="form--input"
+        />
+        <input
+          type="text"
+          placeholder="Bottom text"
+          className="form--input"
+        />
+        <button
+          className="form--button"
+        >
+          Get a new meme image 🖼
+        </button>
+      </form>
+    </main>
   )
 }
