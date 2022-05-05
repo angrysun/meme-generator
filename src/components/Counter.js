7d89ea9048878df8c0c49f305d43049016a7bcfb
Steven Vasquez <s.vasquez85@gmail.com>
2022-05-05 11:15:17 +0900

    added Counter component

---
+++ src/components/Counter.js
@@ -0,0 +1,22 @@
+import useState from 'react'
+
+export default function App() {
+  const [count, setCount] = useState(0)
+
+  function add() {
+    setCount(prevCount => prevCount + 1)
+  }
+
+  const subtract = () => setCount(prevCount => prevCount - 1)
+
+
+  return (
+    <div className="counter">
+      <button className="counter--minus" onClick={subtract}>–</button>
+      <div className="counter--count">
+        <h1>{count}</h1>
+      </div>
+      <button className="counter--plus" onClick={add}>+</button>
+    </div>
+  )
+}
