afe89ff74a721d1962fc93191d0eb09982961125
Steven Vasquez <s.vasquez85@gmail.com>
2022-05-05 12:11:53 +0900

    useState added for img url

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,11 +1,13 @@
 import memesData from "../memesData"
+import { useState } from "react"
 
 export default function Meme() {
+  const [memeImage, setMemeImage] = useState("")
+
   function getMemeImage() {
     const memesArray = memesData.data.memes
     const randomNumber = Math.floor(Math.random() * memesArray.length)
-    const url = memesArray[randomNumber].url
-    console.log(url)
+    setMemeImage(memesArray[randomNumber].url)
   }
 
   return (
@@ -28,6 +30,7 @@ export default function Meme() {
           Get a new meme image 🖼
         </button>
       </div>
+      <img src={memeImage} className="meme--image" alt="meme"/>
     </main>
   )
 }
