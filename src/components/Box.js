978124100280c948ddcf6ddacae3ae93f6e7491b
Steven Vasquez <s.vasquez85@gmail.com>
2022-05-09 17:11:56 +0900

    mapped over boxes array

--- src/components/Box.js
+++ src/components/Box.js
@@ -3,4 +3,14 @@ import { useState } from "react"
 
 export default function Box() {
   const [boxesArray, setBoxesArray] = useState(boxes)
+
+  const boxElements = boxesArray.map(box => (
+    <div className="box" key={box.id}></div>
+  ))
+
+  return (
+    <main>
+      {boxElements}
+    </main>
+  )
 }
