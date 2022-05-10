da98f0923104f8210418b0a8f0adab00bd5ac712
Steven Vasquez <s.vasquez85@gmail.com>
2022-05-10 18:04:36 +0900

    added state to change boxes style

--- src/components/SecondBox.js
+++ src/components/SecondBox.js
@@ -1,9 +1,14 @@
+import { useState } from "react"
+
 export default function SecondBox(props) {
+  const [OnOff, setOnOff] = useState(props.on)
+  const toggleBox = () => setOnOff(!OnOff)
+
   const styles = {
-    backgroundColor: props.on ? "#222222" : "none"
+    backgroundColor: OnOff ? "#222222" : "transparent"
   }
 
   return (
-    <div style={styles} className="box"></div>
+    <div style={styles} className="box" onClick={toggleBox}></div>
   )
 }
