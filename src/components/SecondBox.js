72b74bfad60929aa9bc9ab9e5674027bcd286bf4
Steven Vasquez <s.vasquez85@gmail.com>
2022-05-10 18:18:39 +0900

    changing from local state to unified state rendering

--- src/components/SecondBox.js
+++ src/components/SecondBox.js
@@ -1,14 +1,17 @@
-import { useState } from "react"
-
 export default function SecondBox(props) {
-  const [OnOff, setOnOff] = useState(props.on)
-  const toggleBox = () => setOnOff(!OnOff)
+  // const [OnOff, setOnOff] = useState(props.on)
+  // const toggleBox = () => setOnOff(!OnOff)
 
   const styles = {
-    backgroundColor: OnOff ? "#222222" : "transparent"
+    backgroundColor: props.on ? "#222222" : "transparent"
   }
 
   return (
-    <div style={styles} className="box" onClick={toggleBox}></div>
+    <div
+      style={styles}
+      className="box"
+      onClick={props.toggleBox}
+    >
+    </div>
   )
 }
