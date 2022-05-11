d5ad06f1b52441a0a3590a4927e1838c4958e0a6
Steven Vasquez <s.vasquez85@gmail.com>
2022-05-11 12:21:52 +0900

    declarative function added for toggling boxes

--- src/components/Box.js
+++ src/components/Box.js
@@ -7,21 +7,26 @@ export default function Box(props) {
 
   const toggleBox = (id) => {
     setBoxesArray(prevBoxes => {
-      const newBoxes = []
-      for(let i = 0; i < prevBoxes.length; i++) {
-        const currentBox = prevBoxes[i]
-        if (currentBox.id === id) {
-          const updatedBox = {
-            ...currentBox,
-            on: !currentBox.on
-          }
-          newBoxes.push(updatedBox)
-        } else {
-          newBoxes.push(currentBox)
-        }
-      }
-      return newBoxes
+      return prevBoxes.map((box) => {
+        return box.id === id ? {...box, on: !box.on} : box
+      })
     })
+    // setBoxesArray(prevBoxes => {
+    //   const newBoxes = []
+    //   for(let i = 0; i < prevBoxes.length; i++) {
+    //     const currentBox = prevBoxes[i]
+    //     if (currentBox.id === id) {
+    //       const updatedBox = {
+    //         ...currentBox,
+    //         on: !currentBox.on
+    //       }
+    //       newBoxes.push(updatedBox)
+    //     } else {
+    //       newBoxes.push(currentBox)
+    //     }
+    //   }
+    //   return newBoxes
+    // })
   }
 
   const boxElements = boxesArray.map(box => (
