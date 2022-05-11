8a0e604883c2aa9a353496e84c704a2e4049d4eb
Steven Vasquez <s.vasquez85@gmail.com>
2022-05-11 15:35:26 +0900

    Form component added

---
+++ src/components/Form.js
@@ -0,0 +1,17 @@
+export default function Form() {
+  const [firstName, setFirstName] = React.useState("")
+
+  function handleChange(event) {
+    console.log(event)
+  }
+
+  return (
+    <form>
+      <input
+        type="text"
+        placeholder="First Name"
+        onChange={handleChange}
+      />
+    </form>
+  )
+}
