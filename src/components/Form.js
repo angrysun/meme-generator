72f1d748ad8a2d7e6baaf78272cb397ffa19da0d
Steven Vasquez <s.vasquez85@gmail.com>
2022-05-20 16:41:56 +0900

    handleSubmit function made

--- src/components/Form.js
+++ src/components/Form.js
@@ -25,8 +25,14 @@ export default function Form() {
     })
   }
 
+  function handleSubmit(event) {
+    event.preventDefault()
+    //submitToApi(formData)
+    console.log(formData)
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <input
         type="text"
         placeholder="First Name"
@@ -121,6 +127,9 @@ export default function Form() {
         <option value="indigo">Indigo</option>
         <option value="violet">Violet</option>
       </select>
+      <br />
+      <br />
+      <button>Submit</button>
     </form>
   )
 }
