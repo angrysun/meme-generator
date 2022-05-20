0285312b56dda53589e53f88be75b6c5dc1629d8
Steven Vasquez <s.vasquez85@gmail.com>
2022-05-20 13:59:26 +0900

    radio box added

--- src/components/Form.js
+++ src/components/Form.js
@@ -7,7 +7,8 @@ export default function Form() {
       lastName: "",
       email: "",
       comments: "",
-      isFriendly: true
+      isFriendly: true,
+      employment: ""
     }
   )
 
@@ -64,6 +65,44 @@ export default function Form() {
       {/* htmlFor added for cleaner code and clarity */}
       <label htmlFor="isFriendly">Are you friendly?</label>
       <br />
+
+      <fieldset>
+        <legend>Current employment status</legend>
+
+        <input
+          type="radio"
+          id="unemployed"
+          name="employment"
+          value="unemployed"
+          checked={formData.employment === "unemployed"}
+          onChange={handleChange}
+        />
+        <label htmlFor="unemployed">Unemployed</label>
+        <br />
+
+        <input
+          type="radio"
+          id="part-time"
+          name="employment"
+          value="part-time"
+          checked={formData.employment === "part-time"}
+          onChange={handleChange}
+        />
+        <label htmlFor="part-time">Part-time</label>
+        <br />
+
+        <input
+          type="radio"
+          id="full-time"
+          name="employment"
+          value="full-time"
+          checked={formData.employment === "full-time"}
+          onChange={handleChange}
+        />
+        <label htmlFor="full-time">Full-time</label>
+        <br />
+
+      </fieldset>
     </form>
   )
 }
