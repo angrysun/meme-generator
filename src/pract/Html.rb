ab87fa662db42f3d141fa61875ab738ea4a27933
Steven Vasquez <s.vasquez85@gmail.com>
2022-06-01 10:36:19 +0900

    updated function

--- src/pract/Html.rb
+++ src/pract/Html.rb
@@ -1,6 +1,6 @@
 def html_elements(str)
-  opening_tags = str.match(/<\w+>/g)
-  closing_tags = str.match(/(<\/\w+>)/g).reverse();
+  opening_tags = str.match(/<\w+>/)
+  closing_tags = str.match(/(<\/\w+>)/).reverse();
   strObj = {
     '<div>': '</div>',
     '<p>': '</p>',
@@ -8,6 +8,28 @@ def html_elements(str)
     '<em>': '</em>',
     '<b>': '</b>',
   }
+  max = Math.max(opening_tags.length, closing_tags.length)
 
+  i = 0
+  while i < max
+    if (strObj[openingTags[i]] != closingTags[i])
+      (openingTags[i] || closingTags[i]).replace(/<|>/, '')
+    end
+    i++
+  end
 
+  return true
 end
+
+
+def demo(str)
+  result = html_elements(str)
+  puts "#{str} gives us #{result}"
+end
+
+demo("<div><div><b><b/></div></p>")
+demo("<p><div><b><b/></div></p>")
+demo("<p><div></p></div>")
+demo("<p><div><b></b>")
+demo("<p><div></b></div></p>")
+demo("<p><div><b></b></div></p>")
