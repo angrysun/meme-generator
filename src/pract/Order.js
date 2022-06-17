139f7a22bd8e48c419169aef3c9298f4dd0cfa6b
Steven Vasquez <s.vasquez85@gmail.com>
2022-06-17 16:22:54 +0900

    testing out different functionalities

--- src/pract/Order.js
+++ src/pract/Order.js
@@ -20,17 +20,39 @@ orderByName(toot)
 
 const getReferrers = (list) => {
   const user_list = JSON.parse(JSON.stringify(list))
-  const user_list2 = []
-
-  user_list.forEach(user => {
-    for (let key in user) {
-      console.log(user.name)
-      // user.referrer = user.find((element) => {
-      // }
-    }
-  });
+  const user_list2 = user_list.map(user => (
+    user.referrer = user_list.find((object) => {
+      if (object.id === user.referrerId) {
+        return true;
+      }
+    })
+    , user.referrer = user.referrer && user.referrer.name || null
+  ))
+  return user_list2
 }
 
+  for (i = 0; i < user_list.length; i++) {
+    user_list[i].referrer = user_list.find(function (x) {
+      if (x.id == user_list[i]["referrerId"]) {
+        return true;
+      }
+    })
+    user_list[i].referrer = user_list[i].referrer && user_list[i].referrer.name || null;
+    user_list2.push(user_list[i])
+  }
+  return user_list2
+}
+
+let lips = [
+  { id: 1, referrerId: 2, name: "Joe" },
+  { id: 2, referrerId: null, name: "Jane" },
+]
+
+getReferrers(lips)
+
+const getReferrers = (user_list) => {
+  user_list = JSON.parse(JSON.stringify(user_list))
+  user_list2 = [];
   for (i = 0; i < user_list.length; i++) {
     user_list[i].referrer = user_list.find(function (x) {
       if (x.id == user_list[i]["referrerId"]) {
