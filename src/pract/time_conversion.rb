faef70c539bb4e39696cd89a74ba0b00ff74883e
Steven Vasquez <s.vasquez85@gmail.com>
2022-07-06 18:50:30 +0900

    change to addition

--- src/pract/time_conversion.rb
+++ src/pract/time_conversion.rb
@@ -1,14 +1,9 @@
 def timeConversion(s)
   if s.end_with?('PM')
     converted = s.chomp("PM")
-    case converted.start_with?
-    when "13"
-      convert = converted.gsub('13', "1")
-    when
-
-    else
-
-    end
+    original = converted[0..1]
+    time = (converted[0..1].to_i + 12).to_s
+    convert = converted.gsub(original, time)
   else
     convert = s.chomp("AM")
   end
