b0b965f6b2d272f9aad7803db73e811bb1473ca3
Steven Vasquez <s.vasquez85@gmail.com>
2022-07-11 17:30:11 +0900

    color rendering

---
+++ src/pract/ColorRenderer.js
@@ -0,0 +1,18 @@
+import Color from './Color';
+
+const colors = [{
+  hex: '#61dafb',
+  name: 'Turquoise Blue'
+},
+{
+  hex: '#cd7fbb',
+  name: 'Sky Magenta'
+},
+{
+  hex: '#7b0901',
+  name: 'Dark Burgundy'
+},
+{
+  hex: '#33a58c',
+  name: 'Niagara'
+}]
