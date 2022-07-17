020eba94eea138eab80dee1b867f28c68ecea9fd
Steven Vasquez <s.vasquez85@gmail.com>
2022-07-17 15:40:23 +0900

    if adde

--- src/pract/str_ab.rb
+++ src/pract/str_ab.rb
@@ -1,3 +1,11 @@
 def aThreeFromB(str)
+  letters = []
+  str_array = str.each_char.to_a
+  str_array.each_index do |i|
+    if (str_array[i] == 'a') then
+      letters.push(i+3)
+    end
+  end
+
 
 end
