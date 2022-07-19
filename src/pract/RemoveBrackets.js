d340ceb5ea1d1efb1cfa9775485b596483070bd3
Steven Vasquez <s.vasquez85@gmail.com>
2022-07-19 13:15:37 +0900

    conditional for pushing left and right ( )

--- src/pract/RemoveBrackets.js
+++ src/pract/RemoveBrackets.js
@@ -4,7 +4,11 @@ function RemoveBrackets(str) {
   let right = []
 
   for (let i = 0; i < array.length; i++) {
-
+    if (array[i] === '(') {
+      left.push(i)
+    } else if (array[i] === ')') {
+      right.push(i)
+    }
   }
 
 }
