3e369388e4edf0dff45066b86dc3fa00cbe37e25
Steven Vasquez <s.vasquez85@gmail.com>
2022-07-22 21:11:55 +0900

    refactored and made functions shorter

--- src/pract/str_ab.rb
+++ src/pract/str_ab.rb
@@ -2,9 +2,7 @@ def aThreeFromB(str)
   letters = []
   array = str.each_char.to_a
   array.each_index do |i|
-    if (array[i] === 'a')
-      letters.push(i + 4)
-    end
+    letters.push(i + 4) if array[i] == 'a'
   end
 
   if str[letters[0]] === 'b'
